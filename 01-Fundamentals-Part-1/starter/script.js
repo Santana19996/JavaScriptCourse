039c19a8cd10423f4e7c1f90ed197cfa2fbf6337
Oscar <oscarsantana2014@gmail.com>
2022-01-08 14:52:33 -0600

    Quick Challenge

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,18 @@
 let js = "amazing";
     console.log(40 + 8 + 23 -10);
+
+    let country = "US";
+    let continent = "North America";
+    let population = 3000000;
+
+    console.log(country);
+    console.log(continent);
+    console.log(population);
+
+
+    let isIsland = true;
+
+    let language;
+
+    console.log(typeof isIsland);
+    console.log(typeof language);
